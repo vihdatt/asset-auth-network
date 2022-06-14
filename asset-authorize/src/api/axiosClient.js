8adb01b4d8958d3214d82b7a24ed849092de851b
Đàm Quang Tiến <damtien444@gmail.com>
2022-06-13 19:58:27 -0700

    Clean: Folder cleanup
    Clean up

--- asset-authorize/src/api/axiosClient.js
+++ asset-authorize/src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import queryString from 'query-string';
 
 const axiosClient = axios.create({
-  baseURL: "http://113.174.169.248:4000/",
+  baseURL: "http://localhost:4000/",
   headers: {
     'content-type': 'application/json',
     'Authorization': 'Bearer ' + localStorage.getItem('token')
